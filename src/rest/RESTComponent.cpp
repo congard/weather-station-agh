19a61cfa607bf18360f8df5a1c80d578fcdef90d
Daniel <dbcongard@gmail.com>
2024-05-31 15:31:27 +0200

    RESTComponent: implement helper functions

---
+++ src/rest/RESTComponent.cpp
@@ -0,0 +1,47 @@
+#include "RESTComponent.h"
+#include "core/log.h"
+
+LOG_TAG("RESTComponent");
+
+using namespace expressif::http::server;
+
+void RESTComponent::writeJson(Request &req, const JsonDocument &json, size_t chunkSize) {
+    std::string jsonStr;
+    serializeJson(json, jsonStr);
+
+    auto resp = req.response();
+    resp.setType("application/json");
+    resp.write(jsonStr, chunkSize);
+
+    LOGI("Response: %s", jsonStr.c_str());
+}
+
+void RESTComponent::writeError(Request &req, std::string_view message, size_t chunkSize) {
+    JsonDocument json;
+    json["error"]["message"] = message;
+    writeJson(req, json, chunkSize);
+}
+
+void RESTComponent::readJson(Request &req, const ReadCallback &readCallback) {
+    if (auto opt = req.readAll(); opt.has_value()) {
+        auto &data = opt.value();
+
+        JsonDocument json;
+
+        if (auto result = deserializeJson(json, data); result.code() == DeserializationError::Ok) {
+            readCallback(json);
+        } else {
+            std::string message = "JSON Deserialization error: ";
+            message += result.c_str();
+            message += ", code: ";
+            message += std::to_string(result.code());
+            writeError(req, message);
+        }
+    } else {
+        writeError(req, "Read error");
+    }
+}
+
+void RESTComponent::log(std::string_view message) {
+    LOGI("%s", message.data());
+}
--- src/rest/RESTComponent.h
+++ src/rest/RESTComponent.h
@@ -2,7 +2,11 @@
 #define WEATHERSTATION_RESTCOMPONENT_H
 
 #include <memory>
+#include <sstream>
 #include <tulz/util/noncopyable.h>
+
+#include <expressif/http/server/HTTPServer.h>
+#include <ArduinoJson.h>
 
 class Application;
 class RESTServer;
@@ -12,8 +16,52 @@ public:
     virtual ~RESTComponent() = default;
 
     virtual void enableFor(RESTServer &server) = 0;
+
+protected:
+    using Request = expressif::http::server::Request;
+    using HTTPMethod = expressif::http::server::HTTPMethod;
+
+    using ReadCallback = std::function<void(const JsonDocument&)>;
+
+    static void writeJson(Request &req, const JsonDocument &json, size_t chunkSize = 0);
+    static void writeError(Request &req, std::string_view message, size_t chunkSize = 0);
+    static void readJson(Request &req, const ReadCallback &readCallback);
+
+    /**
+     * @tparam T The type.
+     * @param v The input data.
+     * @return `true` if the key is not specified or,
+     * if specified, it must be of type `T`.
+     */
+    template<typename T>
+    static bool isValidType(const JsonVariantConst &v) {
+        return v.isUnbound() || v.is<T>();
+    }
+
+    /**
+     * Associates the value with the key and logs
+     * the corresponding message.
+     * @tparam T The type.
+     * @param json The json object to append to.
+     * @param key The key.
+     * @param value The value.
+     */
+    template<typename T>
+    static void setAndLog(JsonDocument &json, std::string_view key, T &&value) {
+        json[key] = value;
+
+        std::ostringstream stream;
+        stream << key << " was set to " << value;
+
+        log(stream.view());
+    }
+
+private:
+    static void log(std::string_view message);
 };
 
 using RESTComponentPtr = std::unique_ptr<RESTComponent>;
+
+#define endpointHandler(handler) [this](Request &req) { handler(req); }
 
 #endif //WEATHERSTATION_RESTCOMPONENT_H
